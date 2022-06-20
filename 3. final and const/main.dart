dc84382e84599ea4200bc315e3736f0a1a97123d
Ravi Verma <55842522+raviverma2791747@users.noreply.github.com>
2022-06-20 20:49:45 +0530

    3. and 4.

---
+++ 3. final and const/main.dart
@@ -0,0 +1,26 @@
+class Name {
+    //in class const cannot be used but with static we can use it.
+    static const int a =12;
+
+    //final is allowed
+    final int b = 11;
+
+}
+
+
+void main(){
+    //declaration
+    final int a = 10;
+    final a1 = 10;
+
+    const int b = 10;
+    const b1 = 20;
+
+    //compile time constant
+    const int d = 11;
+
+    //run time constant
+    final int e = 12;
+
+
+}
---
+++ 4. if and else/main.dart
@@ -0,0 +1,20 @@
+void main(){
+    var salary = 2000;
+
+    if(salary >  2000){
+        print("You are promoted !");
+    }else{
+        print("You need to work hard!");
+    }
+
+
+    int number = 100;
+
+    if(number >= 90) {
+        print('A');
+    }else if(number >= 80 && number < 90){
+        print('B');
+    }else{
+        print('C');
+    }
+}
